1c3a168572dbf564e6186f8f36dd8ead85bf0878
Benson Choy <benson.choy@rivirhouse.com>
2024-12-18 21:51:03 +1000

    remoe scripts

--- tools/orchestrator/tmp_scripts/infrastructure_1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'Date: 2024-12-18 21:25:31.387022'
-echo 'Script: ./tmp_scripts/infrastructure_1.sh'
-cd /mnt/d/Work/Intuitas/repo/ambo_sim/infrastructure/confluent
-python3 -c 'print("Firing up confluent")'
-python3 -c "import time; time.sleep(1)"
-docker-compose up -d
